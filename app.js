36564712915ff23f60d3bcf9eee84d38b2b25533
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-17 17:15:38 +0200

    updated the index file

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 let formElement = document.getElementById("submit-form");
 let inputValueElement = document.getElementById("email-input");
 let submitBtn = document.getElementById("submit-btn");
+let emailName = document.getElementById("input-value");
+let container = document.getElementById("container");
 
 function subscribeNow(event) {
   event.preventDefault();
+  let emailRecepient = inputValueElement.value;
+  let newContent = `
+        <img src="img/icon-list.svg" class="closing-icon"/>
+        <h1>
+        Thanks for Subscribing!
+        </h1>
+        <p class="text">
+        A confirmation email has been sent to ${emailRecepient}. 
+        Please open it and click<br/> the button
+        inside to confirm your subscription.
+        </p>
+        <button type="submit" id="closing-btn">
+        Dismiss message
+        </button>
+  `;
+  container.innerHTML = newContent;
+  container.style.display = "block";
+  container.style.width = "300px";
 }
 
-submitBtn.addEventListener("submit", subscribeNow);
+formElement.addEventListener("submit", subscribeNow);
